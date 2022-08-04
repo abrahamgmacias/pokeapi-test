1bba31beaa830e843f92970b52f1638f3afa6951
abrahamgmacias <70555209+abrahamgmacias@users.noreply.github.com>
2022-08-03 20:58:10 -0600

    connects to database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const app = require("./app");
+const { Sequelize } = require("sequelize");
+const config = require("./database/config/config.json")[process.env.NODE_ENV];
 
+const db = new Sequelize("postgres://localhost:5432", config);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+    db.sync();
     console.log(`Listening in port ${PORT}...`);
 });
